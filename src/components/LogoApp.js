74305c877dd23f0cdc024dfba76554e8d491d87d
MatiRdez <matiasnrz5@gmail.com>
2023-05-09 21:52:54 -0300

    Vinculando estilos en componente LogoApp

--- src/components/LogoApp.js
+++ src/components/LogoApp.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ImgLogo from './images/LogoApp.png';
+import './styles/LogoApp.css';
 
 const LogoApp = () => {
     return(
-        <div>
+        <div className='logo'>
             <a href='#'><img src={ImgLogo}></img></a>
         </div>
     )
