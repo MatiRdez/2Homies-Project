5df56ee6029ea2eec279d662d6eaa7af45f939fe
MatiRdez <matiasnrz5@gmail.com>
2023-05-09 21:16:15 -0300

    Desarrollando componente del logo

---
+++ src/components/LogoApp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImgLogo from './images/LogoApp.png';
+
+const LogoApp = () => {
+    return(
+        <div>
+            <a href='#'><img src={ImgLogo}></img></a>
+        </div>
+    )
+}
+
+export default LogoApp;
