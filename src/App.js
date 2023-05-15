710e0a83337ff7fe94d166a41528123d278faad3
MatiRdez <matiasnrz5@gmail.com>
2023-05-15 15:27:00 -0300

    Implementando componente BotonLogin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LogoApp from './components/LogoApp.js';
+import BotonLogin from './components/BotonLogin';
 
 function App() {
   return (
     <div>
       <LogoApp/>
+      <BotonLogin/>
     </div>
   );
 }
