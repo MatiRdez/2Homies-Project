6311780e66316b21cd21898b2a99f3008331fd0f
MatiRdez <matiasnrz5@gmail.com>
2023-05-15 15:23:05 -0300

    Desarrollando componente BotonLogin

---
+++ src/components/BotonLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BotonLogin = () => {
+    return(
+        <div>
+            <input type="button"></input>
+        </div>
+    )
+}
+
+export default BotonLogin;
