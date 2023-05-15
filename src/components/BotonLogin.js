00eef87a0d18e0cb81e0c6425db4256365932bfb
MatiRdez <matiasnrz5@gmail.com>
2023-05-15 15:34:23 -0300

    Creando props para el componente BotonLogin

--- src/components/BotonLogin.js
+++ src/components/BotonLogin.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const BotonLogin = () => {
+const BotonLogin = (props) => {
     return(
         <div>
-            <input type="button"></input>
+            <input type="button" value={props.texto}></input>
         </div>
     )
 }
