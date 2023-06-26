3c58e3fdab2bcd8d847c2f2bd641747b59a1fabf
MatiRdez <matiasnrz5@gmail.com>
2023-06-26 17:39:53 -0300

    Creando lista de opciones

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,1 +1,13 @@
 import React from "react";
+
+const Menu = () => {
+    return(
+        <div>
+            <ul>
+                
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
