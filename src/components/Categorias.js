0ef1fb0245c7fc2bc2dd47ec39183fec206fc868
valensoria <valentin.soria.epet20@gmail.com>
2023-06-27 21:04:44 -0300

    Creando título de Categorias

--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -1,3 +1,8 @@
 import React from "react";
 const Categorias = () => {
     const categorias = ["Ficción", "No ficción", "Ciencia ficción", "Fantasía", "Misterio", "Romance", "Aventura"];
+
+    return (
+        <div>
+          <h2>Categorías</h2>
+          <ul>
