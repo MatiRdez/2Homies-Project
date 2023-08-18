33f618b808cf418c5ec4a287a16900395f3e2339
MatiRdez <matiasnrz5@gmail.com>
2023-08-18 14:44:18 -0300

    Agregando botón para acceder

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -7,6 +7,7 @@ const Login = () => {
             <input id="correo" type="text"></input>
             <label for="pass">Contraseña</label>
             <input id="pass" type="password"></input>
+            <input type="button" value="Acceder"></input>
         </div>
     )
 }
