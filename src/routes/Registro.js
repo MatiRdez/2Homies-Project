288e25180a0a96390596735688dbb5d3477bc0af
MatiRdez <matiasnrz5@gmail.com>
2023-09-14 14:20:47 -0300

    Creación y asignación de variables para registro

--- src/routes/Registro.js
+++ src/routes/Registro.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Registro = () => {
+
+    const [email, setEmail] = useState('');
+    const [usuario, setUsuario] = useState('');
+    const [password, setPassword] = useState('');
+
     return(
         <div>
             <form>
                 <label for="correo">Correo electrónico</label>
-                <input id="correo" type="text"></input>
+                <input id="correo" type="text" value={email}></input>
                 <label for="usuario">Nombre de usuario</label>
-                <input id="usuario" type="text"></input>
+                <input id="usuario" type="text" value={usuario}></input>
                 <label for="pass">Contraseña</label>
-                <input id="pass" type="password"></input>
+                <input id="pass" type="password" value={password}></input>
                 <input id="enviar" type="submit" value="Registrarse"></input>
             </form>
         </div>
