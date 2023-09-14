b88ac452ad14a148cbcd5dfa17c70bbd4108c192
MatiRdez <matiasnrz5@gmail.com>
2023-09-14 13:42:15 -0300

    Agregando ruta de registro

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LogoApp from './components/LogoApp.js';
 import BotonLogin from './components/BotonLogin';
 import Login from './routes/Login';
+import Registro from './routes/Registro';
 import { BrowserRouter, Link, Route, Routes, NavLink } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <NavLink to='/registro'><BotonLogin texto='Registrarse'/></NavLink>
         <Routes>
           <Route path='/login' element={<Login/>}></Route>
+          <Route path='/registro' element={<Registro/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
