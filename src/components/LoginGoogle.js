a9e7afcb7bc0f0f02753528e483a4da813cceb7a
MatiRdez <matiasnrz5@gmail.com>
2023-09-26 14:58:07 -0300

    Importando módulos de firebase en login de Google

--- src/components/LoginGoogle.js
+++ src/components/LoginGoogle.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { auth } from "../firebaseConfig";
+import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
 
 const BotonGoogle = () => {
     return(
