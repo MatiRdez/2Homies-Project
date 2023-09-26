9629232bc18001df189f50dd2f7871922c8e492f
MatiRdez <matiasnrz5@gmail.com>
2023-09-26 15:01:42 -0300

    Creando variable que provee datos de la cuenta

--- src/components/LoginGoogle.js
+++ src/components/LoginGoogle.js
@@ -3,6 +3,9 @@ import { auth } from "../firebaseConfig";
 import { GoogleAuthProvider, signInWithPopup } from "@firebase/auth";
 
 const BotonGoogle = () => {
+
+    const Provider = new GoogleAuthProvider();
+
     return(
         <div>
             <button>Continuar con Google</button>
