d3e93ad42494e875379458cd449c95b98780c1bc
MatiRdez <matiasnrz5@gmail.com>
2023-09-27 13:35:18 -0300

    Creando variable que provee datos de la cuenta de GitHub

--- src/components/LoginGitHub.js
+++ src/components/LoginGitHub.js
@@ -3,6 +3,9 @@ import { auth } from "../firebaseConfig";
 import { GithubAuthProvider, signInWithPopup } from "@firebase/auth";
 
 const BotonGitHub = () => {
+
+    const Provider = new GithubAuthProvider();
+
     return(
         <div>
             <button>Continuar con GitHub</button>
