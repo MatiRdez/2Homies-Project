2aebbaf1a3e9cd5ae2869ea46935015763b408a3
MatiRdez <matiasnrz5@gmail.com>
2023-10-24 13:49:18 -0300

    Creando función para iniciar en anónimo

--- src/components/LoginAnonimo.js
+++ src/components/LoginAnonimo.js
@@ -3,6 +3,11 @@ import { auth } from "../firebaseConfig";
 import { signInAnonymously } from "firebase/auth";
 
 const BotonAnonimo = () => {
+
+    const IniciarAnonimo = () => {
+        
+    }
+
     return(
         <div>
             <button>Continuar como anónimo</button>
