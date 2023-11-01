4a7e9069f3b00a222b488ce0ab70973225fe4843
MatiRdez <matiasnrz5@gmail.com>
2023-11-01 19:48:48 -0300

    Importando archivo CSS para botón de salir

--- src/components/Salir.js
+++ src/components/Salir.js
@@ -2,6 +2,7 @@ import React from "react";
 import { auth } from "../firebaseConfig";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import "./styles/BotonSalir.css";
 
 const Salir = () => {
 
