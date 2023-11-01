cf6933bc602ca61f49215fb370e1319e41f27de7
MatiRdez <matiasnrz5@gmail.com>
2023-11-01 19:28:34 -0300

    Importando módulos de Firebase para salir

--- src/components/Salir.js
+++ src/components/Salir.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { auth } from "../firebaseConfig";
+import { signOut } from "firebase/auth";
 
 const Salir = () => {
     return(
         <div>
-            
+
         </div>
     )
 }
