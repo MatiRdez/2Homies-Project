567d2c905f92df16d182da22510b1244e43cab91
MatiRdez <matiasnrz5@gmail.com>
2023-11-01 19:42:06 -0300

    Implementando componente para cerrar sesión

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Salir from "./Salir";
 
 const Home = (props) => {
     return(
         <>
           <h1>Bienvenidos!</h1>
+          <Salir/>
         </>
     )
 }
