f1daf8fb6c31d03a9708fbfcb48b9d856ebfac05
MatiRdez <matiasnrz5@gmail.com>
2023-11-06 13:49:15 -0300

    Creando arreglo para mostrar libros

--- src/components/ListarLibros.js
+++ src/components/ListarLibros.js
@@ -3,9 +3,12 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 
 const ListarLibros = () => {
+
+    const [libros, setLibros] = useState([]);
+
     return(
         <div>
-            
+
         </div>
     )
 }
