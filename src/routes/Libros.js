3b2552e8a10fc7e9f27cf76e34ee0390af686a4c
MatiRdez <matiasnrz5@gmail.com>
2023-11-06 13:16:40 -0300

    Agregando dato de género del libro en CRUD

--- src/routes/Libros.js
+++ src/routes/Libros.js
@@ -6,16 +6,19 @@ const Libros = () => {
 
     const [titulo, setTitulo] = useState("");
     const [autor, setAutor] = useState("");
+    const [genero, setGenero] = useState("");
 
     const BotonCrearLibro = async () =>{
         try{
             const docRef = await addDoc(collection(db, "Libros"), {
                 Título: titulo,
                 Autor: autor,
+                Género: genero,
             });
             console.log("Documento agregado correctamente con ID: ", docRef.id);
             setTitulo("");
             setAutor("");
+            setGenero("");
         }
         catch(error){
             console.log("Error al agregar el documento: ", error);
@@ -36,6 +39,11 @@ const Libros = () => {
                 value={autor}
                 onChange={(e) => setAutor(e.target.value)}
             />
+            <select value={genero} onChange={(e) => setGenero(e.target.value)}>
+                <option value="Acción">Acción</option>
+                <option value="Aventura">Aventura</option>
+                <option value="Terror">Terror</option>
+            </select>
             <button onClick={BotonCrearLibro}>Agregar libro</button>
         </div>
     )
