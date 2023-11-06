6ea816941536ad76ed961ca17a56fd1111124f85
MatiRdez <matiasnrz5@gmail.com>
2023-11-06 13:48:35 -0300

    Desarrollando componente de listar libros

--- src/components/ListarLibros.js
+++ src/components/ListarLibros.js
@@ -1,3 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebaseConfig";
+
+const ListarLibros = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
