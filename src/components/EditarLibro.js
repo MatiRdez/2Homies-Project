e16774fec31aff7e1ad6e4d61a5c8bb9b704c0d8
MatiRdez <matiasnrz5@gmail.com>
2023-11-06 15:04:35 -0300

    Creando función para editar libro

--- src/components/EditarLibro.js
+++ src/components/EditarLibro.js
@@ -1,3 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { collection, getDoc, updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
+
+const EditarLibro = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
