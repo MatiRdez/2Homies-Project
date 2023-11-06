126c9b79fe706617a8d88e201c777a14359bcae8
MatiRdez <matiasnrz5@gmail.com>
2023-11-06 15:20:34 -0300

    Obteniendo ID del libro a editar

--- src/components/EditarLibro.js
+++ src/components/EditarLibro.js
@@ -2,12 +2,25 @@ import React, { useState, useEffect } from "react";
 import { collection, getDoc, updateDoc, doc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 
-const EditarLibro = () => {
+const EditarLibro = ({libroId}) => {
 
     const [libro, setLibro] = useState({});
     const [nuevoTitulo, setNuevoTitulo] = useState("");
     const [nuevoAutor, setNuevoAutor] = useState("");
     const [nuevoGenero, setNuevoGenero] = useState("");
+
+    useEffect(() => {
+        const obtenerLibro = async () => {
+            const docRef = doc(db, "Libros", libroId);
+            const docSnap = await getDoc(docRef);
+            if(docSnap.exists()){
+                setLibro({ id: docSnap.id, ...docSnap.data() });
+            }else{
+                console.log("Error al encontrar el libro");
+            }
+        };
+        obtenerLibro();
+    }, [libroId]);
 
     return(
         <div>
