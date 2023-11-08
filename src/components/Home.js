725ffc1777084b9e92eec245b3675c020174dfb9
valensoria <valentin.soria.epet20@gmail.com>
2023-11-08 16:35:48 -0300

    Importando componente de Menu en Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,33 @@
 import React from "react";
 import Salir from "./Salir";
 import "./styles/Home.css";
-
+import ScrollToSearchButton from "./Buscar";
+import SearchBooks from "./Buscar";
+import { NavLink } from "react-router-dom";
+import Menu from "./Menu";
 const Home = (props) => {
     return(
         <>
-      <div className="header">
-  <img src="logo.png" alt="Logo" className="logo" />
-  <h1>Bienvenidos!</h1>
-  <label for="toggle">☰</label>
-  <input class="check" type="checkbox" id="toggle" />
-  <div className="navbar">
-    <ul>
-      <li><a href="#">Inicio</a></li>
-      <li><a href="#">Navegar</a></li>
-      <li><a href="#">Libros</a></li>
-      <li><a href="#">Buscar</a></li>
-      <li><Salir/></li>
-    </ul>
-  </div>
-</div>
+         
+          <div className="book-list">
+          <div className="book">
+            <img src="book1.jpg" alt="Libro 1" />
+            <h2>Título del Libro 1</h2>
+            <p>Autor: Autor del Libro 1</p>
+            <p>Descripción del Libro 1</p>
+          </div>
 
-<div className="book-list">
-  <div className="book">
-    <img src="book1.jpg" alt="Libro 1" />
-    <h2>Título del Libro 1</h2>
-    <p>Autor: Autor del Libro 1</p>
-    <p>Descripción del Libro 1</p>
-  </div>
-
-  <div className="book">
-    <img src="book2.jpg" alt="Libro 2" />
-    <h2>Título del Libro 2</h2>
-    <p>Autor: Autor del Libro 2</p>
-    <p>Descripción del Libro 2</p>
-  </div>
-  
+          <div className="book">
+            <img src="book2.jpg" alt="Libro 2" />
+            <h2>Título del Libro 2</h2>
+            <p>Autor: Autor del Libro 2</p>
+            <p>Descripción del Libro 2</p>
+          </div>
+          
 
 
-  
-</div>
+          
+        </div>
 
         </>
     )
