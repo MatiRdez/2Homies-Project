4c1bd2400dc87bfe3980d7aba1829cfe951b64dd
valensoria <valentin.soria.epet20@gmail.com>
2023-11-08 16:40:24 -0300

    Agregando html basico para generos de libros en categorias

--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -7,7 +7,23 @@ const Categorias = () => {
   return (
     <div>
       <Menu></Menu>
-      
+      <div class="container">
+    <div class="genre-box mystery">
+      <p>Misterio</p>
+    </div>
+    <div class="genre-box romance">
+      <p>Romance</p>
+    </div>
+    <div class="genre-box fantasy">
+      <p>Fantasía</p>
+    </div>
+    <div class="genre-box scifi">
+      <p>Ciencia Ficción</p>
+    </div>
+    <div class="genre-box thriller">
+      <p>Thriller</p>
+    </div>
+  </div>
     </div>
   )
 }
