dc29a8fe11781dcd5eb2d768aa71a94bee2a3dbb
MatiRdez <matiasnrz5@gmail.com>
2023-11-08 13:01:05 -0300

    Desarrollando componente para eliminar libro

--- src/components/EliminarLibro.js
+++ src/components/EliminarLibro.js
@@ -2,3 +2,11 @@ import React, { useState, useEffect } from "react";
 import { collection, getDoc, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useParams, useNavigate } from "react-router-dom";
+
+const EliminarLibro = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
