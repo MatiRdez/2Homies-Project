8c87bd295bcc29285be8389f4d46c1efec1e1175
valensoria <valentin.soria.epet20@gmail.com>
2023-11-08 16:37:54 -0300

    Creando Categorias.js e importando react

--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -1,16 +1,9 @@
 import React from "react";
-const Categorias = () => {
-    const categorias = ["Ficción", "No ficción", "Ciencia ficción", "Fantasía", "Misterio", "Romance", "Aventura"];
 
-    return (
-        <div>
-          <h2>Categorías</h2>
-          <ul>
-           {categorias.map((categoria, index) => (
-             <li key={index}>{categoria}</li>
-          ))}
-          </ul>
-    </div>
-  );
+const Categorias = () => {
+  return (
+    
+  )
 }
 export default Categorias;
+
