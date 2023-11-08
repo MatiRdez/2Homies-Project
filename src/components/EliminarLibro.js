e820c3ee4e88a8e4cb0c86cffeef67c870f506bd
MatiRdez <matiasnrz5@gmail.com>
2023-11-08 14:01:32 -0300

    Agregando función para recargar ruta

--- src/components/EliminarLibro.js
+++ src/components/EliminarLibro.js
@@ -3,13 +3,14 @@ import { collection, getDoc, deleteDoc, doc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
 import { useParams, useNavigate } from "react-router-dom";
 
-const EliminarLibro = ({libroId}) => {
+const EliminarLibro = ({ libroId }) => {
 
     const BotonEliminar = async () => {
         try{
             const libroRef = doc(db, "Libros", libroId);
             await deleteDoc(libroRef);
             alert("Libro eliminado con éxito");
+            window.location.reload();
         }
         catch(error){
             console.error("Error al eliminar el libro:", error);
