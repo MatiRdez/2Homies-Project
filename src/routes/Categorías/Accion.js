1f7196ab1d617dfe0c7db2d8f9b3b8740e9b3e70
MatiRdez <matiasnrz5@gmail.com>
2023-11-12 19:42:05 -0300

    Obteniendo libros de acción

--- src/routes/Categorías/Accion.js
+++ src/routes/Categorías/Accion.js
@@ -2,3 +2,37 @@ import React, { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import { Link } from "react-router-dom";
+
+const Accion = () => {
+
+    const [libros, setLibros] = useState([]);
+
+    useEffect(() => {
+        const obtenerLibros = async () => {
+            try{
+                const librosCollection = collection(db, "Libros");
+                const librosSnapshot = await getDocs(librosCollection);
+                const librosAccion = [];
+
+                librosSnapshot.forEach((doc) => {
+                    const libro = doc.data();
+                    if (libro.Género === "Acción") {
+                        librosAccion.push(libro);
+                    }
+                });
+
+                setLibros(librosAccion);
+            } catch (error) {
+                console.error("Error al obtener libros:", error);
+            }
+        };
+
+        obtenerLibros();
+    }, []);
+
+    return(
+        <div>
+
+        </div>
+    )
+}
