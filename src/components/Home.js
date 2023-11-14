63e4c566dee59bcbf118edb3a9937e7a25e5c133
MatiRdez <matiasnrz5@gmail.com>
2023-11-14 13:23:39 -0300

    Actualizando plantilla de estilos del menú

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,29 +7,9 @@ import { NavLink } from "react-router-dom";
 import Menu from "./Menu";
 const Home = (props) => {
     return(
-        <>
-          <Menu></Menu>
-          <div className="book-list">
-          <div className="book">
-            <img src="book1.jpg" alt="Libro 1" />
-            <h2>Título del Libro 1</h2>
-            <p>Autor: Autor del Libro 1</p>
-            <p>Descripción del Libro 1</p>
-          </div>
-
-          <div className="book">
-            <img src="book2.jpg" alt="Libro 2" />
-            <h2>Título del Libro 2</h2>
-            <p>Autor: Autor del Libro 2</p>
-            <p>Descripción del Libro 2</p>
-          </div>
-          
-
-
-          
-        </div>
-
-        </>
+      <div>
+        <Menu/>
+      </div>
     )
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,30 @@
 import React from "react";
 import "./styles/Home.css"
+import ImgLogo from "../components/images/LogoApp.png";
 import SearchBooks from "./Buscar";
 import Salir from "./Salir";
 import { NavLink } from "react-router-dom";
+
 const Menu = () => {
     return(
-      <div className="header">
-      <img src="logo.png" alt="Logo" className="logo" />
-      <h1>Bienvenidos!</h1>
-      <label for="toggle">☰</label>
-      <input class="check" type="checkbox" id="toggle" />
-      <div className="navbar">
-        <ul>
-          <NavLink to='/Home'><li>Inicio</li></NavLink>
-          <NavLink to='/Navegar'><li>Navegar</li></NavLink>
-          <li><a href="#">Libros</a></li>
-          <NavLink to="/listar-libros"><li>CRUD - Libros</li></NavLink>
-          <SearchBooks className="busc"></SearchBooks>
-          <li><Salir/></li>
-        </ul>
-        
-      </div>
+      <div>
+        <div className="header">
+          <img src={ImgLogo} alt="Logo" className="logo" />
+          <h1 className="titulo">Bienvenidos!</h1>
+          <div className="search-container">
+            <input type="text" placeholder="Buscar..." className="search-input" />
+            <button type="button" className="search-button">Buscar</button>
+          </div>
+        </div>
+        <div className="navbar">
+          <ul>
+            <NavLink to='/home'><li>Inicio</li></NavLink>
+            <NavLink to='/navegar'><li>Navegar</li></NavLink>
+            <li>Mi colección</li>
+            <NavLink to="/listar-libros"><li>CRUD - Libros</li></NavLink>
+            <Salir/>
+          </ul>
+        </div>
       </div>
     )
 }
