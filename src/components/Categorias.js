5157a93c088514c6cf8fa31110586a689bb401bd
MatiRdez <matiasnrz5@gmail.com>
2023-11-14 19:03:42 -0300

    Estructurando ruta de categorías

--- src/components/Categorias.js
+++ src/components/Categorias.js
@@ -6,24 +6,33 @@ import "./styles/Categorias.css"
 const Categorias = () => {
   return (
     <div>
-      <Menu></Menu>
+      <Menu/>
       <div class="container">
-    <div class="genre-box mystery">
-      <p>Misterio</p>
+        <div class="categoria-aventura">
+            <h2>Aventura</h2>
+            <p>Explora mundos desconocidos y vive emocionantes peripecias con personajes valientes y decididos.</p>
+        </div>
+        <div class="categoria-acción">
+            <h2>Acción</h2>
+            <p>Sumérgete en historias llenas de adrenalina, combates espectaculares y héroes que enfrentan desafíos extremos.</p>
+        </div>
+        <div class="categoria-terror">
+            <h2>Terror</h2>
+            <p>Si buscas emociones fuertes y escalofríos, elige historias que te mantendrán al borde del asiento, llenas de misterio y suspense.</p>
+        </div>
+        <div class="categoria-fantasía">
+            <h2>Fantasía</h2>
+            <p>Descubre mundos mágicos, criaturas extraordinarias y aventuras épicas que desafían las leyes de la realidad.</p>
+        </div>
+        <div class="categoria-historia">
+            <h2>Historia</h2>
+            <p>Sumérgete en el pasado y vive eventos históricos a través de relatos que te transportarán a diferentes épocas y culturas.</p>
+        </div>
+        <div class="categoria-romance">
+            <h2>Romance</h2>
+            <p>Embárcate en historias de amor apasionadas y conmovedoras, donde los personajes enfrentan desafíos para encontrar el verdadero significado del amor.</p>
+        </div>
     </div>
-    <div class="genre-box romance">
-      <p>Romance</p>
-    </div>
-    <div class="genre-box fantasy">
-      <p>Fantasía</p>
-    </div>
-    <div class="genre-box scifi">
-      <p>Ciencia Ficción</p>
-    </div>
-    <div class="genre-box thriller">
-      <p>Thriller</p>
-    </div>
-  </div>
     </div>
   )
 }
