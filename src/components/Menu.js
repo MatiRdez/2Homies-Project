15330a5c54881c90d311bed5ac692a5c9920b542
valensoria <valentin.soria.epet20@gmail.com>
2023-11-16 15:11:52 -0300

    Adaptando menu.js para la barra de busqueda

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./styles/Menu.css"
 import ImgLogo from "../components/images/LogoApp.png";
-import SearchBooks from "./Buscar";
+
 import Salir from "./Salir";
 import { NavLink } from "react-router-dom";
+import Buscador from "./Buscador";
 
 const Menu = () => {
     return(
@@ -12,14 +13,15 @@ const Menu = () => {
           <img src={ImgLogo} alt="Logo" className="logo" />
           <h1 className="titulo">Bienvenidos!</h1>
           <div className="search-container">
-            <input type="text" placeholder="Buscar..." className="search-input" />
-            <button type="button" className="search-button">Buscar</button>
+           
+            <Buscador/>
           </div>
         </div>
         <div className="navbar">
           <ul>
             <NavLink to='/home'><li>Inicio</li></NavLink>
             <NavLink to='/navegar'><li>Navegar</li></NavLink>
+            
             <li>Mi colección</li>
             <NavLink to="/listar-libros"><li>CRUD - Libros</li></NavLink>
             <Salir/>
