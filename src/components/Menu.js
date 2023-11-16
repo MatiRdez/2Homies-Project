8589b60a5e734506b0d87b665a9aae66cdb3e10e
valensoria <valentin.soria.epet20@gmail.com>
2023-11-16 15:37:28 -0300

    Implementando responsive al menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "./styles/Menu.css"
+import React, { useState } from "react";
+import "./styles/Menu.css";
 import ImgLogo from "../components/images/LogoApp.png";
 
 import Salir from "./Salir";
@@ -7,28 +7,41 @@ import { NavLink } from "react-router-dom";
 import Buscador from "./Buscador";
 
 const Menu = () => {
-    return(
-      <div>
-        <div className="header">
-          <img src={ImgLogo} alt="Logo" className="logo" />
-          <h1 className="titulo">Bienvenidos!</h1>
-          <div className="search-container">
-           
-            <Buscador/>
-          </div>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <div className="menu-toggle" onClick={toggleMenu}>
+          ☰
         </div>
-        <div className="navbar">
-          <ul>
-            <NavLink to='/home'><li>Inicio</li></NavLink>
-            <NavLink to='/navegar'><li>Navegar</li></NavLink>
-            
-            <li>Mi colección</li>
-            <NavLink to="/listar-libros"><li>CRUD - Libros</li></NavLink>
-            <Salir/>
-          </ul>
+        <img src={ImgLogo} alt="Logo" className="logo" />
+        <h1 className="titulo">Bienvenidos!</h1>
+        <div className="search-container">
+          <Buscador />
         </div>
       </div>
-    )
-}
+      <div className={`navbar ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <NavLink to="/home" onClick={toggleMenu}>
+            <li>Inicio</li>
+          </NavLink>
+          <NavLink to="/navegar" onClick={toggleMenu}>
+            <li>Navegar</li>
+          </NavLink>
+          <li>Mi colección</li>
+          <NavLink to="/listar-libros" onClick={toggleMenu}>
+            <li>CRUD - Libros</li>
+          </NavLink>
+          <Salir onClick={toggleMenu} />
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Menu;
